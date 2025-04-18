80a74356bb7eea9bb5fb30ec53b8d176c24c06a4
Jeffy <wonderjeffy@gmail.com>
2025-04-18 17:57:55 +0800

    Initial Commit

---
+++ ChineseColorsApp/ChineseColorsAppApp.swift
@@ -0,0 +1,21 @@
+//
+//  ChineseColorsAppApp.swift
+//  ChineseColorsApp
+//
+//  (\(\
+//  ( -.-)
+//  o_(")(")
+//  -----------------------
+//  Created by jeffy on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ChineseColorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChineseColorsAppTests/ChineseColorsAppTests.swift
@@ -0,0 +1,20 @@
+//
+//  ChineseColorsAppTests.swift
+//  ChineseColorsAppTests
+//
+//  (\(\
+//  ( -.-)
+//  o_(")(")
+//  -----------------------
+//  Created by jeffy on 4/18/25.
+//
+
+import Testing
+
+struct ChineseColorsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
